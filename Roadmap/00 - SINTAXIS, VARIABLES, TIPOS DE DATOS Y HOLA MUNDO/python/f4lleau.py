7567790e7a41872ad1a0ee3dad98b5825881ee3a
F4lleau <ivonnefalleau@gmail.com>
2024-06-03 08:18:29 -0300

    ejecicio 0

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/f4lleau.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/f4lleau.py
@@ -5,3 +5,18 @@
 comentario en varias lineas
 comentario en varias lineas
 """
+
+str_color='Red'
+str_color='Blue'
+
+#en python no hay CONSTANTES, una práctica es escribir la var con MAYÚSC para que se entienda que no hay que modificarla
+
+#tipos de datos
+
+my_string="string"
+my_int= 38
+my_float= 38.5
+my_boolean= True
+my_boolean= False
+
+print ("Hola, es mi primer mensaje")
